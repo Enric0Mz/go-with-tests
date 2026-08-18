c931e2ae1c617a323d5d7589d040db7590cb2fb1
Enrico Marquez <enrico.marquez@sumup.com>
2026-08-18 16:31:33 -0300

    feat: add example of Repeat method

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,6 +1,7 @@
 package iteration
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -17,4 +18,9 @@ func BenchmarkLoop(b *testing.B) {
 	for b.Loop() {
 		Repeat("a", 50)
 	}
+}
+
+func ExampleRepeat() {
+	fmt.Println(Repeat("b", 4))
+	// Output: bbbb
 }
