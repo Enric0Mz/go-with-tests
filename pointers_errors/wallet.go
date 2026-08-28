8d6d2175a1c5c55a303b6f7a96f168a7a08ea8fd
Enrico Marquez <enrico.marquez@sumup.com>
2026-08-28 15:30:21 -0300

    feat: finalyze pointers and errors module

--- pointers_errors/wallet.go
+++ pointers_errors/wallet.go
@@ -1,13 +1,63 @@
-package pointerserrors
+package main
 
-type Wallet struct {
-	balance int
+import (
+	"errors"
+	"fmt"
+)
+
+var withdrawErr = errors.New("Insuficient balance to withdraw")
+
+type Stringer interface {
+	String() string
 }
 
-func (w *Wallet) Deposit(amount int) {
+type Wallet struct {
+	balance Bitcoin
+}
+
+type Bitcoin int
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return withdrawErr
+	}
+	w.balance -= amount
+	return nil
+}
+
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func ProcessWithdraw(amount Bitcoin, accountID string, wallet *Wallet) error {
+	err := wallet.Withdraw(amount)
+
+	if err != nil {
+		return fmt.Errorf("account %s has insufficient funds in wallet: %w", accountID, err)
+	}
+	return nil
+}
+
+func main() {
+	wallet := Wallet{Bitcoin(10)}
+
+	err := ProcessWithdraw(100, "acc-xyz", &wallet)
+
+	if err == withdrawErr {
+		fmt.Print("Conventional IF WORKS")
+	} else {
+		fmt.Printf("Conventional IF DOES NOT WORK")
+	}
+
+	if errors.Is(err, withdrawErr) {
+		fmt.Printf("This is the correct way to check wrapped errors")
+	}
 }
--- pointers_errors/wallet_test.go
+++ pointers_errors/wallet_test.go
@@ -1,4 +1,4 @@
-package pointerserrors
+package main
 
 import (
 	"testing"
@@ -7,13 +7,43 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
+	t.Run("should add 10 bitcoins to balance when .Deposit(10)", func(t *testing.T) {
+		wallet := Wallet{}
 
-	wallet.Deposit(10)
+		wallet.Deposit(Bitcoin(10))
 
-	expect := 10
+		expect := Bitcoin(10)
 
-	actual := wallet.Balance()
+		actual := wallet.Balance()
 
-	require.Equal(t, expect, actual)
+		require.Equal(t, expect, actual)
+	})
+	t.Run("should withdraw 10 bitcoins of balance when .Withdraw()", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(20)}
+
+		err := wallet.Withdraw(Bitcoin(10))
+		require.NoError(t, err)
+		expect := Bitcoin(10)
+		actual := wallet.Balance()
+
+		require.Equal(t, expect, actual)
+	})
+
+	t.Run("should return error when withdrawing more bitcoins then total balance", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(10)}
+
+		err := wallet.Withdraw(Bitcoin(200))
+		require.Error(t, err)
+		require.Equal(t, withdrawErr, err)
+
+	})
+
+	t.Run("should return 10 BTC when printing a bitcoin of 10", func(t *testing.T) {
+		btc := Bitcoin(10)
+
+		expect := "10 BTC"
+		actual := btc.String()
+
+		require.Equal(t, expect, actual)
+	})
 }
