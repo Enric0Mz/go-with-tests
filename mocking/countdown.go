2676572137cbeb8a2b3c7cecbd7acc228bebe0df
Enrico Marquez <enricovmarquezz@gmail.com>
2026-09-09 13:59:27 -0300

    feat(mock): implement Sleeper mocks

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -7,19 +7,37 @@ import (
 	"time"
 )
 
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+type DefaultSleeper struct{}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
 const countDonwStart = 3
 const finalLine = "Go!"
 
-func Countdown(writter io.Writer) {
+func Countdown(writter io.Writer, sleeper Sleeper) {
 
 	for i := countDonwStart; i > 0; i-- {
 		fmt.Fprintln(writter, i)
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
 	fmt.Fprint(writter, finalLine)
 
 }
 
 func main() {
-	Countdown(os.Stdout)
+	Countdown(os.Stdout, &DefaultSleeper{})
 }
--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -9,15 +9,17 @@ import (
 
 func TestCountdown(t *testing.T) {
 	t.Run("Print 3", func(t *testing.T) {
+		spySleeper := &SpySleeper{}
 		buffer := bytes.Buffer{}
 		expect := `3
 2
 1
 Go!`
-		Countdown(&buffer)
+		Countdown(&buffer, spySleeper)
 
 		actual := buffer.String()
 
 		require.Equal(t, expect, actual)
+		require.Equal(t, 3, spySleeper.Calls)
 	})
 }
